41c929579a42637b6347034ba5018eb9bf349133
ayang007 <86279159+ayang007@users.noreply.github.com>
2024-05-13 10:33:31 -0700

    cardio, breathing rate, and az added. most json returning None, will look into calibration

--- orm.py
+++ orm.py
@@ -1,6 +1,6 @@
 import requests
 import urllib, json
-from datetime import date
+from datetime import timedelta, date
 import sqlite3
 from auth import authentication
 
@@ -21,16 +21,67 @@ def generic_fetch(user_id, user_token, json_extension, date):
 
     return response
 
+# generic fetch function -> returns json response
+def period_fetch(user_id, user_token, json_extension, date, period):
+    user_header = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer ' + user_token,
+    }
+
+    # test: grabs today's weight
+    # json_extension should be something like '/body/log/weight/date/'
+    response = requests.get('https://api.fitbit.com/1/user/' + user_id + json_extension + str(date) + period + '.json', headers=user_header)
+
+    return response
+
 # reformat sleep data
 def sleep(user_id, user_token, date):
     response = generic_fetch(user_id, user_token, '/sleep/date/', date)
-    values = json.loads(response.content)
-    
-def temp(user_id, user_token, date):
-    response = generic_fetch(user_id, user_token, '/temp/skin/date/', date)
+    print("sleep:")
     print(response)
     values = json.loads(response.content)
     print(values)
+    
+def temp(user_id, user_token, date):
+    response = generic_fetch(user_id, user_token, '/temp/skin/date/', date)
+    print("skintemp:")
+    print(response)
+    #values = json.loads(response.content)
+    #print(values)
+
+def cardio(user_id, user_token, date):
+    response = generic_fetch(user_id, user_token, '/spo2/date/', date)
+    print("spo2:")
+    print(response)
+    response = generic_fetch(user_id, user_token, '/hrv/date/', date)
+    print("hrv:")
+    values2 = json.loads(response.content)
+    print(values2)
+    response = generic_fetch(user_id, user_token, '/cardioscore/date/', date)
+    print("vo2:")
+    print(response)
+    response = generic_fetch(user_id, user_token, '/ecg/list', date)
+    print("ecg:")
+    print(response)
+
+def breathrate(user_id, user_token, date):
+    response = generic_fetch(user_id, user_token, '/br/date/', date)
+    print("breathing rate summary:")
+    print(response)
+
+def activityzone(user_id, user_token, date):
+    response = period_fetch(user_id, user_token, '/activities/active-zone-minutes/date/', date, '1d')
+    print("az summary:")
+    print(response)
+    response = generic_fetch(user_id, user_token, '/activities/date/', date)
+    print("activity summary:")
+    print(response)
 
 mom_token = authentication('C2R3WZ')
-print(temp('C2R3WZ', mom_token, date.today()))
+for i in range(1,2): # fetch data for the week
+    # make daily entry in dailyTable
+    print(sleep('C2R3WZ', mom_token, date.today() - timedelta(i)))
+    print(cardio('C2R3WZ', mom_token, date.today() - timedelta(i)))
+    print(breathrate('C2R3WZ', mom_token, date.today() - timedelta(i)))
+    print(activityzone('C2R3WZ', mom_token, date.today() - timedelta(i)))
+    print(temp('C2R3WZ', mom_token, date.today() - timedelta(i)))
